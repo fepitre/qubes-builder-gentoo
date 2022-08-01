456ecc6c0f54b616a450b01ff8dc8c9877e06e88
Frédéric Pierret (fepitre) <frederic.pierret@qubes-os.org>
2022-08-01 09:56:44 +0200

    Python 3.10 is now the default
    https://www.gentoo.org/support/news-items/2022-06-13-python3-10.html

--- scripts/02_install_groups.sh
+++ scripts/02_install_groups.sh
@@ -22,11 +22,20 @@ setPortageProfile "${INSTALLDIR}" "${TEMPLATE_FLAVOR}"
 # Standard Gentoo flags
 setupBaseFlags "${INSTALLDIR}" "${TEMPLATE_FLAVOR}"
 
+# Ensure to upgrade to python3.10
+cat > "${INSTALLDIR}/etc/portage/package.use/python3.10" << EOF
+*/* PYTHON_TARGETS: -* python3_10
+*/* PYTHON_SINGLE_TARGET: -* python3_10
+EOF
+
 # Update Portage
 updatePortage "${INSTALLDIR}"
 
 # Ensure chroot is up to date
 updateChroot "${INSTALLDIR}"
+
+# Once update is finished with forced python3.10 upgrade remote USE file
+rm "${INSTALLDIR}/etc/portage/package.use/python3.10"
 
 # Standard Gentoo packages to install
 installBasePackages "${INSTALLDIR}" "${TEMPLATE_FLAVOR}"
